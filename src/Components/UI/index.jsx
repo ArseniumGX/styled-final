ec194015c947736d7a1e3d5f2eadacd5bcd1ac43
José Pereira Macedo <arseniumgx@gmail.com>
2023-08-18 17:27:22 -0300

    feat(aula3): refatoração

--- src/Components/UI/index.jsx
+++ src/Components/UI/index.jsx
@@ -1,4 +1,11 @@
-.btn {
+import styled from "styled-components";
+
+export const Icone = styled.img`
+  height: 25px;
+  width: 25px;
+`;
+
+export const Botao = styled.button`
   margin: 15px auto 0px auto;
   display: block;
   border-radius: 20px;
@@ -9,9 +16,9 @@
   font-size: 14px;
   padding: 8px 20px;
   cursor: pointer;
-}
+`;
 
-.box {
+export const Box = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -21,25 +28,19 @@
   padding: 20px;
   width: 48%;
   height: 260px;
-}
 
-.saldo {
-  font-weight: 700;
-  font-size: 32px;
-}
-
-.detalhe {
-  color: #41d3be;
-  font-size: 24px;
-}
-
-@media (max-width: 800px) {
-  .box {
+  @media (max-width: 800px) {
     width: 95%;
     margin: 5px;
   }
+`;
 
-  .conteudo {
-    flex-direction: column;
-  }
-}
+export const Saldo = styled.div`
+  font-weight: 700;
+  font-size: 32px;
+`;
+
+export const Detalhe = styled.span`
+  color: #41d3be;
+  font-size: 24px;
+`;
